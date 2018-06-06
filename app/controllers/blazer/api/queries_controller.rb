dff766b5d167ad21a6f554cda483bfabd4ad5ee2
Francisco Martins <franciscomxs@gmail.com>
2018-06-06 14:50:08 -0300

    Use a hashes instead of arrays with one element

--- app/controllers/blazer/api/queries_controller.rb
+++ app/controllers/blazer/api/queries_controller.rb
@@ -6,6 +6,8 @@ module Blazer
           Blazer::Query.find(params[:id]).statement
         ].all
 
+        hashes = hashes.first if hashes.one?
+
         render json: JSON.generate(hashes), status: 200
       end
     end
